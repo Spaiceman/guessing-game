e802c298ceee3b6c57452f9b85e91307c1cb3ba0
Spaiceman <76124405+Spaiceman@users.noreply.github.com>
2021-07-05 01:17:05 +0300

    feat: task cpmpleted

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
 
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.max + this.min) / 2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
 
